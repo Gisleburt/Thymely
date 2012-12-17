4ef5ee9ec9e2716d2f810bc776926f9055d816ab
Gisleburt <daniel@danielmason.com>
2012-12-17 05:52:18 +0000

    Autogenerated LazyData classes added

---
+++ Library/Thymely/LazyData/ThymelyUsers.php
@@ -0,0 +1,66 @@
+<?php 
+
+	use Thymely\LazyData;
+
+	/**
+	 * LazyData class
+	 */
+	class ThymelyUsers extends thymelyAbstract {
+		
+		/**
+		 * User Id 
+		 * @var long 
+		 */
+		public user_id;
+
+		
+		/**
+		 * Email 
+		 * @var string 
+		 */
+		public email;
+
+		
+		/**
+		 * Password 
+		 * @var string 
+		 */
+		public password;
+
+		
+		/**
+		 * Firstname 
+		 * @var string 
+		 */
+		public firstname;
+
+		
+		/**
+		 * Lastname 
+		 * @var string 
+		 */
+		public lastname;
+
+		
+		/**
+		 * Date Created 
+		 * @var string 
+		 */
+		public date_created;
+
+		
+		/**
+		 * Date Modified 
+		 * @var string 
+		 */
+		public date_modified;
+
+		
+		/**
+		 * Date Deleted 
+		 * @var string 
+		 */
+		public date_deleted;
+
+		
+	}
